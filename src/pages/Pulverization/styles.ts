afccea2259f93480f0791beec5b70fafc1f387eb
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-03-19 15:50:54 -0300

    Added Font RoboboSlab

--- src/pages/Pulverization/styles.ts
+++ src/pages/Pulverization/styles.ts
@@ -2,7 +2,11 @@ import styled from 'styled-components/native';
 
 const Container = styled.View``;
 
-const Title = styled.Text``;
+const Title = styled.Text`
+  font-size: 20px;
+  color: #3d3d4d;
+  font-family: 'RobotoSlab-Bold';
+`;
 
 export { Container, Title };
 
