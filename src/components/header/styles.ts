354bc4e3bf6bd906f13d0011c63d56efa41b5179
Bruna Oruchi <brunaquispeo@gmail.com>
2021-03-21 16:39:03 -0300

    Created component header

---
+++ src/components/header/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #3c8dbc;
+  padding: 16px 32px;
+`;
+
+export const Text = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 30px;
+  color: #ffffff;
+  padding-top: 8px;
+`;
