4f84fd3e706e8931c18a6b2c65a069f56e245f5e
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2021-03-22 18:04:09 -0300

    Updated component buttonText

--- src/components/buttonText/index.tsx
+++ src/components/buttonText/index.tsx
@@ -7,15 +7,23 @@ interface ButtonProps extends RectButtonProperties {
   children: string;
   color: 'red' | 'yellow' | 'green' | 'gray';
   size: 'small' | 'large';
+  disable?: boolean;
 }
 
 const ButtonText: React.FC<ButtonProps> = ({
   children,
   color,
   size,
+  disable = false,
   ...rest
 }) => (
-  <Container color={color} size={size} {...rest}>
+  <Container
+    color={color}
+    size={size}
+    enabled={!disable}
+    disable={disable}
+    {...rest}
+  >
     <Text>{children}</Text>
   </Container>
 );
--- src/components/buttonText/styles.ts
+++ src/components/buttonText/styles.ts
@@ -4,6 +4,7 @@ import { RectButton } from 'react-native-gesture-handler';
 interface ContainerProps {
   color: 'red' | 'yellow' | 'green' | 'gray';
   size: 'small' | 'large';
+  disable: boolean;
 }
 
 export const Container = styled(RectButton)<ContainerProps>`
@@ -11,6 +12,7 @@ export const Container = styled(RectButton)<ContainerProps>`
   border-radius: 4px;
   justify-content: center;
   align-items: center;
+  margin: 8px 0;
 
   ${props => {
     if (props.size === 'large') {
@@ -24,21 +26,24 @@ export const Container = styled(RectButton)<ContainerProps>`
   }}
 
   ${props => {
-    if (props.color === 'red') {
-      return css`
-        background-color: #d14a61;
-      `;
+    if (!props.disable) {
+      if (props.color === 'red') {
+        return css`
+          background-color: #d14a61;
+        `;
+      }
+      if (props.color === 'yellow') {
+        return css`
+          background-color: #ffc042;
+        `;
+      }
+      if (props.color === 'green') {
+        return css`
+          background-color: #00da6d;
+        `;
+      }
     }
-    if (props.color === 'yellow') {
-      return css`
-        background-color: #ffc042;
-      `;
-    }
-    if (props.color === 'green') {
-      return css`
-        background-color: #00da6d;
-      `;
-    }
+
     return css`
       background-color: #9f9faf;
     `;
